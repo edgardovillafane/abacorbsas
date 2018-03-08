0c330a8cf3f92f39b3281e73983637ccdbb6cf94
eze <ezequiel.r.gallo@gmail.com>
2018-03-08 17:48:06 -0300

    estilado jueves

--- src/app/mock-beneficios.ts
+++ src/app/mock-beneficios.ts
@@ -2,9 +2,9 @@ import { Beneficios } from './beneficios-class';
 
 export const BENEFICIOS : Beneficios[] = [
     
-    {id: 1, nombre: 'Arquitectos', imagen:"/assets/img/impermeable.png" },
-    {id: 2, nombre: 'Comercios', imagen:"/assets/img/premium.png" },
-    {id: 3, nombre: 'Instaladores', imagen:"/assets/img/membranacement.png" },
-    {id: 4, nombre: 'Distribuidores', imagen:"/assets/img/multifunc.png" }
+    {id: 1, nombre: 'Arquitectos', imagen:"/assets/img/jarron.jpeg" },
+    {id: 2, nombre: 'Comercios', imagen:"/assets/img/jarron.jpeg" },
+    {id: 3, nombre: 'Instaladores', imagen:"/assets/img/jarron.jpeg" },
+    {id: 4, nombre: 'Distribuidores', imagen:"/assets/img/jarron.jpeg" }
    
 ];
